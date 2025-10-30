7cdd5883efee26a4ecd1a3439752864b7f010c42
Weslleyqueiroz <weslleyqueiroz773@gmai.com>
2025-10-29 20:54:58 -0400

    att

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/index.html",
-    "./src/js/script.js",
-    ".src/input.css",
-    "./aula-12/css/output.css",
-    "./aula-12/css/styles.css",
-    "./src/**/*.{js,ts,jsx,tsx}"
+    "./src/**/*.{js,jsx,ts,tsx}",
+    "./public/index.html"
   ],
   theme: {
     extend: {},
   },
   plugins: [],
 }
-
